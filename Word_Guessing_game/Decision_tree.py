37fef74368c010d5822f4352005e5aac5f6df2c0
Shreyansh Sinha <yashsinha321@gmail.com>
2023-09-04 14:56:33 +0530

    Contains Decision Tree for Word-guessing Game

---
+++ Word_Guessing_game/Decision_tree.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# You are not allowed to import any libraries other than numpy
+
+# SUBMIT YOUR CODE AS A SINGLE PYTHON (.PY) FILE INSIDE A ZIP ARCHIVE
+# THE NAME OF THE PYTHON FILE MUST BE submit.py
+# DO NOT INCLUDE OTHER PACKAGES LIKE SKLEARN, SCIPY, KERAS ETC IN YOUR CODE
+# THE USE OF PROHIBITED LIBRARIES WILL RESULT IN PENALTIES
+
+# DO NOT CHANGE THE NAME OF THE METHOD my_fit BELOW
+# IT WILL BE INVOKED BY THE EVALUATION SCRIPT
+# CHANGING THE NAME WILL CAUSE EVALUATION FAILURE
+
+# You may define any new functions, variables, classes here
+# For example, classes to create the Tree, Nodes etc
+
+################################
+# Non Editable Region Starting #
+################################
+def my_fit( words, num_words ):
+################################
+#  Non Editable Region Ending  #
+################################
+
+	# Use this method to train your decision tree model using the word list provided
+	# Return the trained model as is -- do not compress it using pickle etc
+	# Model packing or compression will cause evaluation failure
+	
+	return model					# Return the trained model
